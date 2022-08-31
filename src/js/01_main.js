2d1e0c3be53822a9df3d53bfb8fb1a2bb58880b7
Denis Tyukavin <dtyukavin@dmvdesk.com>
2022-09-01 00:33:40 +0300

    header fixed and fixed bugs #2

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -1,5 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+	const header = document.querySelector(".header:not(.header--fixed)");
+	const heightHeader = header.offsetHeight;
+	const main = document.querySelector(".main");
+
+	headerFixed();
+
+	window.addEventListener("scroll", headerFixed, false);
+
+	function headerFixed() {
+		const heightOffset = window.pageYOffset;
+
+		if (heightOffset > 0) {
+			main.style.paddingTop = `${heightHeader}px`;
+			header.classList.add("header--fixed");
+		} else if(heightOffset == 0) {
+			main.style.paddingTop = 0;
+			header.classList.remove("header--fixed");
+		}
+	}
+
 	const swiperHero = new Swiper('.hero-slider', {
 		slidesPerView: 1,
 		pagination: {
@@ -55,48 +75,71 @@ document.addEventListener("DOMContentLoaded", () => {
 	});
 
 	(function() {
-		const header = document.querySelector(".header");
-
-		headerFixed();
-
-		function headerFixed() {
-			let heightOffset = window.pageXOffset;
-
-			console.log(heightOffset)
-
-			if (heightOffset > 0) {
-				header.classList.add("header-fixed");
-			} else {
-				header.classList.remove("header-fixed");
-			}
-		}
-	})();
-
-	(function() {
 		const btnMenuMobile = document.querySelector(".js-menu-mobile-button");
 		const menuMobile = document.querySelector(".js-menu-mobile")
+		const iconMenu = document.querySelector(".icon-menu")
+		const iconCancel = document.querySelector(".icon-cancel")
 		let isOpened = false;
 
-		btnMenuMobile.addEventListener("touchstart", (event)=>{
+		btnMenuMobile.addEventListener("touchend", (event)=>{
 			if (isOpened) {
 				menuMobile.classList.remove("is-open");
+				iconMenu.style.display = "block";
+				iconCancel.style.display = "none";
 				isOpened = false;
 			} else {
 				menuMobile.classList.add("is-open");
+				iconMenu.style.display = "none";
+				iconCancel.style.display = "block";
 				isOpened = true;
 			}
 		});
 	})();
 
 	(function() {
+		const headerInnerItems = document.querySelectorAll(".header-menu__item--inner");
+		const innerMenus = document.querySelectorAll(".inner-menu");
+		const headerWrapper = document.querySelector(".header-menu__wrap");
+		const headerBack = document.querySelector(".header-menu-back");
+		const headerBackBtn = document.querySelector(".btn-menu-back");
+
+		headerInnerItems.forEach((item) => {
+			item.addEventListener("touchend", function(event) {
+				event.stopPropagation();
+
+				const innerMenu = event.currentTarget.querySelector(".inner-menu");
+				const heightInnerMenu = innerMenu.offsetHeight;
+
+				headerWrapper.style.height = `${heightInnerMenu}px`;
+				headerBack.style.display = "block";
+			});
+		});
+
+		headerBackBtn.addEventListener("touchend", function(event) {
+			headerWrapper.style.height = "auto";
+			innerMenus.forEach((innerMenu) => {
+				innerMenu.classList.remove("is-show");
+			});
+			headerBack.style.display = "none";
+			event.stopPropagation();
+		}, false);
+	})();
+
+	(function() {
 		const headerInnerItem = document.querySelector(".header-menu__item--inner");
 
 		headerInnerItem.addEventListener("mouseenter", (event)=>{
-			const innerMenu = event.target.querySelector(".inner-menu");
+			const innerMenu = event.currentTarget.querySelector(".inner-menu");
 			innerMenu.classList.add("is-show");
 		});
+		headerInnerItem.addEventListener("touchend", (event)=>{
+			if (event.currentTarget.classList.contains("header-menu__item--inner")) {
+				const innerMenu = event.currentTarget.querySelector(".inner-menu");
+				innerMenu.classList.add("is-show");
+			}
+		});
 		headerInnerItem.addEventListener("mouseleave", (event)=>{
-			const innerMenu = event.target.querySelector(".inner-menu");
+			const innerMenu = event.currentTarget.querySelector(".inner-menu");
 			innerMenu.classList.remove("is-show");
 		});
 	})();
